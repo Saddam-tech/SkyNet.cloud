95d41a452dcfe68a5b90f83e11d64db3e9e42ce2
Saddam-tech <salokhiddinov0727@gmail.com>
2020-08-01 09:01:58 +0900

    some changes being made

---
+++ src/components/Navigation/Toolbar.js
@@ -0,0 +1,2 @@
+//////so today we I have to get started from doing the route to the notePad class based component,
+////// then making a Toolbar and Css of it combined all in one Toolbar css(navigation all stuff).
