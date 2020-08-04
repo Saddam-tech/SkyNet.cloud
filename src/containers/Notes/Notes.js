3b345e3d021faf547d51e151cc938de5c2bf8db0
Saddam-tech <salokhiddinov0727@gmail.com>
2020-08-04 15:32:56 +0900

    eee

--- src/containers/Notes/Notes.js
+++ src/containers/Notes/Notes.js
@@ -29,7 +29,7 @@ class Notes extends Component {
   };
 
   addInputHandler = () => {
-    this.props.history.replace('/notes/NotePad')
+    this.props.history.replace('/notes/NotePad.html 200')
   };
 
   render() {
